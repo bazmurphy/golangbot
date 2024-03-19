4c49d81c46a1ffeecde972ecb1a12890c1fc25b4
Baz <61154071+bazmurphy@users.noreply.github.com>
2024-03-19 11:40:11 +0000

    21-goroutines

---
+++ 21-goroutines/01/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func hello() {
+	fmt.Println("Hello world goroutine")
+}
+
+func main() {
+	go hello()
+	time.Sleep(1 * time.Second)
+	fmt.Println("main function")
+}
+
+// Hello world goroutine
+// main function
---
+++ 21-goroutines/02/go.mod
@@ -0,0 +1,3 @@
+module goroutines_02
+
+go 1.22.1
