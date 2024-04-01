5d0be49c872c853195eec1a077e730fb42b6ac65
Baz <61154071+bazmurphy@users.noreply.github.com>
2024-04-01 14:55:16 +0100

    additions

---
+++ 26-structs-instead-of-classes/01/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "oop/employee"
+
+func main() {
+	e := employee.Employee{
+		FirstName:   "Sam",
+		LastName:    "Adolf",
+		TotalLeaves: 30,
+		LeavesTaken: 20,
+	}
+	e.LeavesRemaining()
+}
+
+// Sam Adolf has 10 leaves remaining
